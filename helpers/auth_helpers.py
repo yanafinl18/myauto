2bd945575de57cc2f2749fcd6aa999c2ea740833
finl <yana.shangaraeva@akbarsdigital.ru>
2020-09-28 18:21:09 +0300

    не нашла как передать sessiontoken  в requests

--- helpers/auth_helpers.py
+++ helpers/auth_helpers.py
@@ -9,27 +9,24 @@ session = {
     'SessionToken': None
 }
 
-def get_auth(session):
+def get_auth():
     data = {'Login': protas['Login'], 'Password': protas['Password']}
     r = requests.post(host + akbars_online_auth_login_init, data=data)
     assert r.status_code == 200
     session['AkbarsLoginOperationId'] = r.json()['Result']['AkbarsLoginOperationId']
 
 
-def confirm_auth(session):
+def confirm_auth():
     data = {"AkbarsOnlineLoginOperationId": session['AkbarsLoginOperationId'],
             "DeviceToken": "2a9f3045-ef42-49ac-b538-c80eb7b5dabc"}
     r = requests.post(host + akbars_online_auth_login_confirm, data=data)
     session['RefreshToken'] = r.json()['Result']['RefreshToken']
 
 
-def get_token(session):
+def get_token():
     data = {"DeviceToken": "2a9f3045-ef42-49ac-b538-c80eb7b5dabc", "GeoLocation": {}, "RefreshToken": session['RefreshToken']}
     r = requests.post(host + auth_create_session, data=data)
     session['SessionToken'] = r.json()['Result']['SessionToken']
     print('SessionToken: ', session['SessionToken'])
 
 
-get_auth(session)
-confirm_auth(session)
-get_token(session)
--- tests/address.py
+++ tests/address.py
@@ -1,8 +1,16 @@
+import requests
 from data.endpoints import find_address
+from helpers.auth_helpers import get_auth, confirm_auth, get_token
+from config import host
 
-def get_address(session['SessionToken']):
+
+get_auth()
+confirm_auth()
+get_token()
+
+def get_address():  # как в request передать sessionToken
     data = {'SearchTerm': 'г Казань ул Чистопольская 11', 'MaxCount': 5}
-    r = requests.post(host + find_address, data=data)
+    r = requests.post(host + find_address, data=data, headers={'SessionToken': session['SessionToken']})
     assert r.status_code == 200
     assert r.json()['Result'][0]['StreetFias'] == "63b26927-9a86-4b80-bba9-02b37135c686", "Запрос на получение адреса не прошел или fias сменился "
     assert r.json()['Result'][0]['House'] == "11"
@@ -10,3 +18,4 @@ def get_address(session['SessionToken']):
     assert r.json()['Result'][0]['Granularity'] == 8
 
 
+get_address()
