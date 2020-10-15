463b7b36e494760eb7dc93f74aae005417c5e9f1
finl <yana.shangaraeva@akbarsdigital.ru>
2020-10-15 23:59:42 +0300

    добавлен параметризованный http

--- helpers/auth_helpers.py
+++ helpers/auth_helpers.py
@@ -1,26 +1,24 @@
-import requests
 from data.endpoints import akbars_online_auth_login_init, akbars_online_auth_login_confirm, auth_create_session
 from data.users import protas
-from config import host
+from models.http import parametrized_post
 
 
 def get_auth(session):
     data = {'Login': protas['Login'], 'Password': protas['Password']}
-    r = requests.post(host + akbars_online_auth_login_init, data=data)
+    r = parametrized_post(endpoint=akbars_online_auth_login_init, body_payload=data)
     session['AkbarsLoginOperationId'] = r.json()['Result']['AkbarsLoginOperationId']
 
 
 def confirm_auth(session):
     data = {"AkbarsOnlineLoginOperationId": session['AkbarsLoginOperationId'],
             "DeviceToken": "2a9f3045-ef42-49ac-b538-c80eb7b5dabc"}
-    r = requests.post(host + akbars_online_auth_login_confirm, data=data)
+    r = parametrized_post(endpoint=akbars_online_auth_login_confirm, body_payload=data)
     session['RefreshToken'] = r.json()['Result']['RefreshToken']
 
 
 def get_token(session):
     data = {"DeviceToken": "2a9f3045-ef42-49ac-b538-c80eb7b5dabc", "GeoLocation": {}, "RefreshToken": session['RefreshToken']}
-    r = requests.post(host + auth_create_session, data=data)
+    r = parametrized_post(endpoint=auth_create_session, body_payload=data)
     session['SessionToken'] = r.json()['Result']['SessionToken']
     print('SessionToken: ', session['SessionToken'])
-
 
--- models/http.py
+++ models/http.py
@@ -1,20 +1,20 @@
 import requests
+from config import host
 
-# что такое url_payload ?
-# что нужно сделать с expected_response_codes
 
-def parametrized_get(endpoint=None, url_payload=None, header_payload=None, expected_response_codes=None, host=None,
-                     timeout=60):
+def parametrized_get(endpoint=None, url_payload=None, header_payload=None, expected_response_codes=[200, 300, 301],
+                     host=host, timeout=60):
 
     response = requests.get(host + endpoint, params=url_payload, headers=header_payload, timeout=timeout)
-    assert response.status_code == expected_response_codes or 200, "Success!"
+    assert response.status_code in expected_response_codes, "Success!"
     return response
 
+
 def parametrized_post(endpoint=None, url_payload=None, data=None, body_payload=None, files=None, header_payload=None,
-                      expected_response_codes=None, host=None, timeout=60):
+                      expected_response_codes=[200, 300, 301], host=host, timeout=60):
     response = requests.post(host + endpoint, params=url_payload, headers=header_payload, data=data, json=body_payload,
                              files=files, timeout=timeout)
-    assert response.status_code == expected_response_codes or 200, "Success!"
+    assert response.status_code in expected_response_codes, "Success!"
     return response
 
 
