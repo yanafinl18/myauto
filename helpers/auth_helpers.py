c906057bbc97bb342768059d10988579a5d8de33
finl <yana.shangaraeva@akbarsdigital.ru>
2020-10-21 20:38:26 +0300

    fix4

--- helpers/auth_helpers.py
+++ helpers/auth_helpers.py
@@ -1,14 +1,15 @@
 from data.endpoints import akbars_online_auth_login_init, akbars_online_auth_login_confirm, auth_create_session, akbars_online_send_otp, get_otp_code
-from data.users import protas
+from data.users import reimond
 from models.http import parametrized_post, parametrized_get
 from data.external_variables import default_device_token
 
 
 def get_auth(session):
-    data = {'Login': protas['Login'], 'Password': protas['Password']}
+    data = {'Login': reimond['Login'], 'Password': reimond['Password']}
     r = parametrized_post(endpoint=akbars_online_auth_login_init, body_payload=data)
     session['AkbarsLoginOperationId'] = r.json()['Result']['AkbarsLoginOperationId']
-    if r.json()['Result']['NeedOtp'] == True:
+    session['NeedOtp'] = r.json()['Result']['NeedOtp']
+    if session['NeedOtp'] == True:
         send_otp(session)
         get_otp(session)
 
@@ -20,7 +21,7 @@ def send_otp(session):
     r = parametrized_post(endpoint=akbars_online_send_otp, body_payload=data)
     assert r.json()['Result']['Phone'] is not None, "Otp don't send"
 
-    print(r.json()['Result']['Phone'])#для отладки
+    print("Phone: ",r.json()['Result']['Phone'])#для отладки
 
 def get_otp(session):
     data = {'operationToken': 'IdentityAbo:'+session['AkbarsLoginOperationId']}
@@ -28,11 +29,15 @@ def get_otp(session):
     assert r.json()['code'] is not None, "Otp code don't exist"
     session['OtpCode'] = r.json()['code']
 
-    print(r.json()['code'])
+    print("Code: ", r.json()['code'])
 
 def confirm_auth(session):
-    data = {"AkbarsOnlineLoginOperationId": session['AkbarsLoginOperationId'],
+    if session['NeedOtp'] == True:
+        data = {"AkbarsOnlineLoginOperationId": session['AkbarsLoginOperationId'],
             "DeviceToken": default_device_token, "OtpCode": session['OtpCode']}
+    else:
+        data = {"AkbarsOnlineLoginOperationId": session['AkbarsLoginOperationId'],
+                "DeviceToken": default_device_token}
     r = parametrized_post(endpoint=akbars_online_auth_login_confirm, body_payload=data)
     session['RefreshToken'] = r.json()['Result']['RefreshToken']
 
