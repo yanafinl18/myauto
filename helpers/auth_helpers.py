b2575650fbbca47d559ddd04b8af0c1c199da0e4
finl <yana.shangaraeva@akbarsdigital.ru>
2020-10-26 22:44:30 +0300

    все сломано 2

--- helpers/auth_helpers.py
+++ helpers/auth_helpers.py
@@ -1,52 +1,69 @@
 from data.endpoints import akbars_online_auth_login_init, akbars_online_auth_login_confirm, auth_create_session, akbars_online_send_otp, get_otp_code
-from data.users import protas, reimond
 from models.http import parametrized_post, parametrized_get
 from data.external_variables import default_device_token
+from data.users import protas
 from bs4 import BeautifulSoup
 import requests
 import re
 
-class Session():
-    def __init__(self, current_user, last_auth_time, session_key, session{}):
+class Session:
+    def __init__(self, current_user):
         self.current_user = current_user
-        self.last_auth_time = last_auth_time
-        self.session = session{}
-        self.session_key = session_key
-
-
+        self.last_auth_time = None
+        self.session_key = None
+        self.session = {
+            'DeviceToken': default_device_token
+        }
 
     def CreateSession(self):
-        session["user"] = protas
         get_auth(self.session)
         confirm_auth(self.session)
         get_token(self.session)
+        return self.session_key
+
 
 
     def get_auth(self):
         data = {'Login': self.current_user['Login'], 'Password': self.current_user['Password']}
         r = parametrized_post(endpoint=akbars_online_auth_login_init, body_payload=data)
-        session['AkbarsLoginOperationId'] = r.json()['Result']['AkbarsLoginOperationId']
-        session['NeedOtp'] = r.json()['Result']['NeedOtp']
-        if session['NeedOtp'] == True:
-            send_otp(session)
-            get_otp(session)
+        self.session['AkbarsLoginOperationId'] = r.json()['Result']['AkbarsLoginOperationId']
+        self.session['NeedOtp'] = r.json()['Result']['NeedOtp']
+        if self.session['NeedOtp'] == True:
+            send_otp(self.session)
+            get_otp(self.session)
 
 
     def send_otp(self):
-        data = {'AkbarsOnlineLoginOperationId': session['AkbarsLoginOperationId']}
+        data = {'AkbarsOnlineLoginOperationId': self.session['AkbarsLoginOperationId']}
         r = parametrized_post(endpoint=akbars_online_send_otp, body_payload=data)
         assert r.json()['Result']['Phone'] is not None, "Otp don't send"
 
 
     def get_otp(self):
-        data = {'operationToken': 'IdentityAbo:'+session['AkbarsLoginOperationId']}
+        data = {'operationToken': 'IdentityAbo:'+self.session['AkbarsLoginOperationId']}
         r = parametrized_get(endpoint=get_otp_code, url_payload=data)
         assert r.json()['code'] is not None, "Otp code don't exist"
-        session['OtpCode'] = r.json()['code']
+        self.session['OtpCode'] = r.json()['code']
 
 
 
+    def confirm_auth(self):
+        if self.session['NeedOtp'] == True:
+            data = {"AkbarsOnlineLoginOperationId": self.session['AkbarsLoginOperationId'],
+                    "DeviceToken": self.session['DeviceToken'], "OtpCode": self.session['OtpCode']}
+        else:
+            data = {"AkbarsOnlineLoginOperationId": self.session['AkbarsLoginOperationId'],
+                    "DeviceToken": self.session['DeviceToken']}
+        r = parametrized_post(endpoint=akbars_online_auth_login_confirm, body_payload=data)
+        self.session['RefreshToken'] = r.json()['Result']['RefreshToken']
 
+
+    def get_token(self):
+        data = {"DeviceToken": self.session['DeviceToken'], "GeoLocation": {}, "RefreshToken": self.session['RefreshToken']}
+        r = parametrized_post(endpoint=auth_create_session, body_payload=data)
+        self.session_key = r.json()['Result']['SessionToken']
+
+"""
 # попытка получения otp с веб страницы
     def get_otp_from_web(self):
         r = requests.get("https://testbankok.akbars.ru/6f1f60ba")
@@ -57,29 +74,13 @@ class Session():
         print(otp2[0])
         reg = re.findall('[0-9]+', str(otp2[0]))  # выборка цифр
         print("OTPCode: ", reg)
-        session['OtpCode'] = reg
+        self.session['OtpCode'] = reg
+"""
 
 
 
-    def confirm_auth(self):
-        if session['NeedOtp'] == True:
-            data = {"AkbarsOnlineLoginOperationId": session['AkbarsLoginOperationId'],
-                    "DeviceToken": default_device_token, "OtpCode": session['OtpCode']}
-        else:
-            data = {"AkbarsOnlineLoginOperationId": session['AkbarsLoginOperationId'],
-                    "DeviceToken": default_device_token}
-        r = parametrized_post(endpoint=akbars_online_auth_login_confirm, body_payload=data)
-        session['RefreshToken'] = r.json()['Result']['RefreshToken']
-
-    def get_token(session):
-        data = {"DeviceToken": default_device_token, "GeoLocation": {}, "RefreshToken": session['RefreshToken']}
-        r = parametrized_post(endpoint=auth_create_session, body_payload=data)
-        session['SessionToken'] = r.json()['Result']['SessionToken']
-        print('SessionToken: ', session['SessionToken'])
-
-
-
-
-
-
-
+session = Session(protas)
+print(session.current_user) # Напечатает protas
+print(session.session_key) # Напечатает созданную сессию
+session.CreateSession()
+print(session.session_key) # вернет новое значение ключа
--- tests/test_address.py
+++ tests/test_address.py
@@ -1,23 +1,19 @@
 from data.endpoints import find_address
 from data.data_address import address_1
-from helpers.auth_helpers import get_auth, confirm_auth, get_token
+#from helpers.auth_helpers import get_auth, confirm_auth, get_token
 from helpers.auth_helpers import Session
 from models.http import parametrized_post
 from data.external_variables import default_user_agent
+from data.users import protas
 
 
 
 session = Session(protas)
-#session = {}
-#get_auth(session)
-#confirm_auth(session)
-#get_token(session)
+
+def test_get_address(self, session):
 
 
-
-def test_get_address():
-
-    r = parametrized_post(endpoint=find_address, body_payload=address_1, header_payload={'SessionToken': session['SessionToken'],
+    r = parametrized_post(endpoint=find_address, body_payload=address_1, header_payload={'SessionToken': session_key,
                                                                'Content-Type': 'application/json; charset=utf-8',
                                                                'User-Agent': default_user_agent
                                                                })
